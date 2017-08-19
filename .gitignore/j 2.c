8111c3838cea23f2624e3bd2ab6e10e4f4a9f1b7
922514114046jani <30343501+922514114046jani@users.noreply.github.com>
2017-08-19 16:40:58 +0530

    Update and rename .gitignore to j 2.c

---
+++ .gitignore/j 2.c
@@ -0,0 +1,13 @@
+#includ<(stdio.h>
+int main()
+{ 
+int num;
+scanf("%d",&num);
+printf(" enter a number");
+if ( num %2==0)
+printf("%d is even",num);
+else
+printf("%d is odd",num);
+return 0;
+}
+
